a3eb7c410642a2b8a2a6008d1ec056c9a8c36a2e
Hasnainsumbal <106953268+Hasnainsumbal@users.noreply.github.com>
2022-06-06 12:49:28 +0500

    Add files via upload

---
+++ ConstructorCheckingB.java
@@ -0,0 +1,16 @@
+class Addition
+{
+int x,y;
+ Addition()
+{ 
+ x=a;
+ y=b;
+} 
+public class ConstructorCheckingB
+{
+  public ststic void main(String args[]){
+  Addition P = new Addition(10,20);
+  system.out.println(P.x+P.Y);
+  system.out.println(Q.x+Q.Y);
+   }
+ }
