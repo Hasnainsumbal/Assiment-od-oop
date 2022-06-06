b4df322a91b3667b6625939f6139768218c2e50a
Hasnainsumbal <106953268+Hasnainsumbal@users.noreply.github.com>
2022-06-06 12:50:42 +0500

    Add files via upload

---
+++ MethodoverloadB.java
@@ -0,0 +1,15 @@
+class Addition
+{
+int x,y;
+ Addition()
+{ 
+  x=10;
+  y=20
+} 
+public class ConstructorCheckingB
+{
+  public ststic void main(string args[]){
+Addition P = new Addition();
+system.out.println(P.x+P.Y);
+   }
+ }
