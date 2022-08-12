c88c261e01e75e333c7231f3a0c11621aef9b607
Hasnainsumbal <106953268+Hasnainsumbal@users.noreply.github.com>
2022-08-12 11:26:32 +0500

    Add files via upload

---
+++ FirstGUI2.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class FirstGUI2
+{
+	public static void main (String args[])
+	{
+		JFrame frm = new JFrame("My first GUI");
+		frm.setSize(600,500);//width, height
+		frm.setLayout(null);
+		frm.setVisible(true);
+	}
+}
